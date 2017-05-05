d610cfb096a179377750f64a5c2de44d2f2cbfb3
Adam Lessen <lessena@gmail.com>
2017-05-04 21:14:02 -0700

    Remove ip filtering

--- server/middleware.js
+++ server/middleware.js
@@ -10,7 +10,7 @@ const ips = [process.env.API_SERVER_IP];
 module.exports = function middleware(app) {
   app.use(logger('dev'));
 
-  app.use('/api/getLocations', ipfilter(ips, { mode: 'allow' }));
+  // app.use('/api/getLocations', ipfilter(ips, { mode: 'allow' }));
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
